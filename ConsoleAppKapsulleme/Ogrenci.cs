10d66e42fb4fd80fb970cab63cc79100b5cbabbf
Korayckr <koraycakir1@hotmail.com>
2023-05-30 11:47:31 +0300

    Add project files.

---
+++ ConsoleAppKapsulleme/Ogrenci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKapsulleme
+{
+    class Ogrenci
+    {
+
+        // Kapsüllemede ana amaç normalde private olan bir değişkeni istenilen noktalarda kullanabilmek
+        // Bunun içinse GET ve SET isimli methodlarda tanımlanan değişkene private olan değişken eşlenir ve gönderilir.
+        private string isim;
+
+
+
+        public void setIsim(string _isim)
+        {
+            isim = _isim;  
+
+        }
+
+        public string getIsim()
+        {
+            return isim;
+        }
+
+
+    }
+}
---
+++ ConsoleAppKapsulleme/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKapsulleme
+{
+    class Program
+    {
+
+        static void Main(String[] args)
+        {
+
+            // Bu kısımda da ilk oluşturulan öğrenci classından türerilen nesneyle get set üzerinden private değişkeni kullanıldı.
+            Ogrenci ogrenci1 = new Ogrenci();
+
+            ogrenci1.setIsim("Koray");
+
+            string gelen = ogrenci1.getIsim();
+
+            if (gelen == "Koray Çakır")
+            {
+                Console.WriteLine("Öğrenci'nin Adı: Koray, Soyadi: Çakır");
+            }
+            else
+            {
+                Console.WriteLine("Öğrenci'nin Adı: Koray, Soyadi: Çakır degildir");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
